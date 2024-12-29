83dc745c1e1fb401d40395675a0156e0ce09dcfc
Tausif Faiyaz <tausif@Tausifs-MacBook-Air.local>
2024-12-29 17:32:06 +0000

    Comments added

--- dataanalysis.py
+++ dataanalysis.py
@@ -1,8 +1,10 @@
 print("hello world")
 
-import pandas as pd
+import pandas as pd # importing libraries to use when coding graphs
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('animals_info.csv')
+data = pd.read_csv('animals_info.csv') #importing data set
 print("Here are the first few animals")
-print(data.head())
+print(data.head()) #testing to see if dataset has been imported
+
+print("Comments added")
