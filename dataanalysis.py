0d99f96b8b5223264ca8ab9eab8efa2c8b2be2ec
Tausif Faiyaz <tausif@Tausifs-MacBook-Air.local>
2024-12-29 17:43:54 +0000

    Clean data by handling missing values

--- dataanalysis.py
+++ dataanalysis.py
@@ -7,4 +7,4 @@ data = pd.read_csv('animals_info.csv') #importing data set
 print("Here are the first few animals")
 print(data.head()) #testing to see if dataset has been imported
 
-print("Comments added")
+data = data.dropna()  # Handle missing values
